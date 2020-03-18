2e6fa8d5c8cf826f8b5b57267a68b36c9127d867
João Victor <albjoaov@gmail.com>
2020-03-18 01:30:54 -0300

    feat (master): Creating project

---
+++ src/test/java/com/mercadolibrewannabe/MercadoLibreWannabeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mercadolibrewannabe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MercadoLibreWannabeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
