7a57b679d734090187fc3fede37460b55fc440e9
João Victor Albuquerque <albjoaov@gmail.com>
2020-03-28 00:21:01 -0300

    [register-user] style: moving comments

--- src/main/java/com/mercadolibrewannabe/config/SwaggerConfig.java
+++ src/main/java/com/mercadolibrewannabe/config/SwaggerConfig.java
@@ -17,7 +17,7 @@ public class SwaggerConfig {
 		return new Docket(DocumentationType.SWAGGER_2)
 				.select()
 				.apis(RequestHandlerSelectors.basePackage("com.mercadolibrewannabe"))
-				// .paths(PathSelectors.ant("/api/**")) TODO: Don't working with base URL
+				.paths(PathSelectors.ant("/**")) // TODO: Don't working with base URL
 				.build();
 
 		/* TODO: at production profile add -> .ignoredParameterTypes with User.class to avoid showing
