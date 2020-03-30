ca2226351064189fb2289b60334a34d57d86bcc6
João Victor Albuquerque <albjoaov@gmail.com>
2020-03-30 04:04:51 -0300

    infra (master): remove SecurityAutoConfiguration

--- src/main/java/com/mercadolibrewannabe/MercadoLibreWannabeApplication.java
+++ src/main/java/com/mercadolibrewannabe/MercadoLibreWannabeApplication.java
@@ -2,8 +2,9 @@ package com.mercadolibrewannabe;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
 public class MercadoLibreWannabeApplication {
 
 	public static void main(String[] args) {
