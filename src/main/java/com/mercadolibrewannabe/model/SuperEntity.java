1a04740e240dcda2e3d4cae22cb16993cc306129
João Victor Albuquerque <albjoaov@gmail.com>
2020-04-05 05:33:46 -0300

    refactor (category-registration): add uuid as string

--- src/main/java/com/mercadolibrewannabe/model/SuperEntity.java
+++ src/main/java/com/mercadolibrewannabe/model/SuperEntity.java
@@ -1,5 +1,6 @@
 package com.mercadolibrewannabe.model;
 
+import org.hibernate.annotations.Type;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
@@ -18,6 +19,7 @@ import java.util.UUID;
 public class SuperEntity {
 
 	@Id
+	@Type(type="uuid-char")
 	@GeneratedValue
 	private UUID id;
 
