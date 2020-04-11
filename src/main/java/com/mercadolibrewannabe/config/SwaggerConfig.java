c2ec01885c9867cace685e9b7cdcb86067efee06
João Victor Albuquerque <albjoaov@gmail.com>
2020-04-11 02:19:36 -0300

    feat (new-product): add swagger config for JWT

--- src/main/java/com/mercadolibrewannabe/config/SwaggerConfig.java
+++ src/main/java/com/mercadolibrewannabe/config/SwaggerConfig.java
@@ -2,11 +2,15 @@ package com.mercadolibrewannabe.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ParameterBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.schema.ModelRef;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.Collections;
 
 @Configuration
 @EnableSwagger2
@@ -18,7 +22,15 @@ public class SwaggerConfig {
 				.select()
 				.apis(RequestHandlerSelectors.basePackage("com.mercadolibrewannabe"))
 				.paths(PathSelectors.ant("/**")) // TODO: Don't working with base URL
-				.build();
+				.build()
+				.globalOperationParameters(Collections.singletonList(
+						new ParameterBuilder()
+								.name("Authorization")
+								.description("Header para token JWT")
+								.modelRef(new ModelRef("string"))
+								.parameterType("header")
+								.required(false)
+								.build()));
 
 		/* TODO: at production profile add -> .ignoredParameterTypes with User.class to avoid showing
 		restricted models */
